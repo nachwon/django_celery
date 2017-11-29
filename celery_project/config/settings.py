f1d5d2114dccbb51b652a2454cdd1b6f74fd8660
Che1 <nachwon@gmail.com>
2017-11-29 16:51:54 +0900

    settings.py edited;

--- celery_project/config/settings.py
+++ celery_project/config/settings.py
@@ -31,7 +31,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'nachwon'
-EMAIL_HOST_PASSWORD = 'skcodnjs132'
+EMAIL_HOST_PASSWORD = '********'
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
